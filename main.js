8184c620860183d383aa8bf41763c0240669557a
SoulRiaper <soulriaper@gmail.com>
2023-04-25 09:46:44 +0300

    beautify js file

--- main.js
+++ main.js
@@ -1,50 +1,49 @@
 const xValues = [50,60,70,80,90,100,110,120,130,140,150];
 const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-const canvaNode = document.getElementById("myChart").getContext('2d');
+const canvaNode = document.getElementById("myChart"); // находим в html элемент графика (у него id = myChart)
 
-chart = new Chart(canvaNode, {
+chart = new Chart(canvaNode, { // создаем новый элемент Chart.js (библиотека для графиков) и задаем его свойства
   type: "line",
   data: {
-    labels: xValues,
+    labels: xValues, // массив шкалы
     datasets: [{
-        label: "Мое чсв",
+        label: "Мое чсв", // название графика
         borderColor: "rgba(0, 255, 200 , 0.3 )",
         fill: false,
-        data: yValues
+        data: yValues // задаем массив данных
     }]
   },
   options:{
-    legend:{
+    legend:{ // не показывать легенду графиков
       display: false,
     }
   }
 });
 
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-}
-
-function removeOne(chart) {
-  chart.data.labels.shift();
-  chart.data.datasets.forEach((dataset)=> {
-    dataset.data.shift();
-  })
-  chart.update();
-}
-
 let xMax = 150;
-let yRand = 10;
-let i = 10;
+let yRand;
 
-function updateData() {
+function updateData() { // функция добавляет рандомные значения в график
   xMax += 10;
   yRand = Math.floor(Math.random() * 20);
   addData(chart, xMax, yRand);
   removeOne(chart);
 }
 
+// функции для добавления новой точки, удаления первой точки (чтоб не засорять график можно использовать обе, таким образом колличество точек всегда будет одинаковым)
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
+  });
+  chart.update();
+}
+
+function removeOne(chart) {
+chart.data.labels.shift();
+chart.data.datasets.forEach((dataset)=> {
+  dataset.data.shift();
+})
+chart.update();
+}
